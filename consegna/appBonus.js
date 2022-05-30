16b1334382de1651dcd2b5cc210a3c30c1beba3d
bia9400 <ferro.biagio@gmail.com>
2022-05-30 16:10:21 +0200

    Prima versione...da migliorare

--- consegna/appBonus.js
+++ consegna/appBonus.js
@@ -3,50 +3,78 @@ const refreshButton = document.getElementById("refresh-button");
 const playButton = document.getElementById("play-button")
 const selectDifficult = document.getElementById("selectInput")
 let numeroOrizontale;
+let bombArray = [];
+let gameOver=false;
+function generateBomb(bombNumber) {
+    
+    for (let i = 0; i < bombNumber; i++) {
+        let numeroGenerato = Math.floor(Math.random() * 100) + 1
+        if (!bombArray.includes(numeroGenerato)) {
+            bombArray.push(numeroGenerato)
+        }
+        else{
+            i--
+        }
+    }
+return bombArray
+}
 
 
 playButton.addEventListener("click", function () {
-    gridContainerBonus.innerHTML=""
-const difficolta = parseInt(selectDifficult.value);
-gridContainerBonus.style.width="0";
+    let punteggio=0;
+    const bombNumber=16;
+    let y=generateBomb(bombNumber);
+    console.log(y);
+    gridContainerBonus.innerHTML = ""
+    const difficolta = parseInt(selectDifficult.value);
+    gridContainerBonus.style.width = "0";
     console.log(difficolta);
-    if(difficolta==0){
-        numeroOrizontale=0;
-        gridContainerBonus.style.width="0";
+    if (difficolta == 0) {
+        numeroOrizontale = 0;
+        gridContainerBonus.style.width = "0";
     }
-    else if(difficolta==1){
-        numeroOrizontale=10
-        let x=numeroOrizontale*70 +1
-        gridContainerBonus.style.width=`${x}px`
+    else if (difficolta == 1) {
+        numeroOrizontale = 10
+        let x = numeroOrizontale * 70 + 1
+        gridContainerBonus.style.width = `${x}px`
     }
-    else if(difficolta==2){
-        numeroOrizontale=9
-        let x=numeroOrizontale*70 +1
-        gridContainerBonus.style.width=`${x}px`
+    else if (difficolta == 2) {
+        numeroOrizontale = 9
+        let x = numeroOrizontale * 70 + 1
+        gridContainerBonus.style.width = `${x}px`
     }
-    else if(difficolta==3){
-        numeroOrizontale=7
-        let x=numeroOrizontale*70 +1
-        gridContainerBonus.style.width=`${x}px`
+    else if (difficolta == 3) {
+        numeroOrizontale = 7
+        let x = numeroOrizontale * 70 + 1
+        gridContainerBonus.style.width = `${x}px`
     }
 
 
 
     function createGrid(numeroOrizontale) {
-        
+
         console.log(numeroOrizontale);
         const numeroTotale = numeroOrizontale * numeroOrizontale
-        
-        
-         
+
+
+
         for (let i = 1; i <= numeroTotale; i++) {
             const elementoHtml = document.createElement("div")
             elementoHtml.classList.add("number-container")
-            elementoHtml.innerHTML = `<h6> ${i} </h6>`
+            
             gridContainerBonus.append(elementoHtml)
             elementoHtml.addEventListener("click", function () {
                 let x = this;
-                x.classList.add("add-color-onClick")
+                
+                if(y.includes(i)){
+                    x.classList.add("bomb")
+                    alert("Hai perso!"+ punteggio)
+                }
+                else{
+                    x.classList.toggle("add-color-onClick")
+                    punteggio++
+                }
+               
             })
 
 
@@ -57,7 +85,7 @@ gridContainerBonus.style.width="0";
         }
     }
 
-createGrid(numeroOrizontale)
+    createGrid(numeroOrizontale)
 })
 
 
